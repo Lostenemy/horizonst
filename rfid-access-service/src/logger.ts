7fd91b115baf09d770dfc91a61acc0f08219221d
Lostenemy <91193835+Lostenemy@users.noreply.github.com>
2025-10-30 09:29:04 +0100

    Use createRequire for Pino logger initialization

--- rfid-access-service/src/logger.ts
+++ rfid-access-service/src/logger.ts
@@ -1,16 +1,23 @@
-import pino from 'pino';
+import { createRequire } from 'node:module';
+import type { Logger, LoggerOptions } from 'pino';
 import { config } from './config.js';
 
-export const logger = pino({
+const require = createRequire(import.meta.url);
+const pino: typeof import('pino') = require('pino');
+
+const options: LoggerOptions = {
   level: config.logLevel,
   base: undefined,
-  transport: process.env.NODE_ENV === 'development'
-    ? {
-        target: 'pino-pretty',
-        options: {
-          colorize: true,
-          translateTime: 'SYS:standard'
+  transport:
+    process.env.NODE_ENV === 'development'
+      ? {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            translateTime: 'SYS:standard'
+          }
         }
-      }
-    : undefined
-});
+      : undefined
+};
+
+export const logger: Logger = pino(options);
